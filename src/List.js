2c95463561b973c0055ded0df0133b3ed86b47dd
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-07-29 13:06:50 +0200

    Added form and input

--- src/List.js
+++ src/List.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function List() {
+  const [item, setItem] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    updateList(item);
+  }
+
+  function updateList(event) {
+    setItem(event.target.value);
+  }
+
   return (
     <div className="List">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="search" placeholder="Enter item" />
-        <input type="submit" value="Add" />
+        <input type="submit" value="Add" onChange={updateList} />
       </form>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   );
 }
